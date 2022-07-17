1de72af5c9e04a781e94e2d50277666b6d8a49dc
ritik jain <ritik.200027@gmail.com>
2022-07-17 18:00:05 +0530

    bug fixes

--- lib/Screens/CartScreen/components/productCard.dart
+++ lib/Screens/CartScreen/components/productCard.dart
@@ -13,45 +13,49 @@ class CartProductCard extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Container(
             height: getProportionateScreenHeight(88),
             width: getProportionateScreenWidth(88),
             padding: const EdgeInsets.all(10),
-            decoration: BoxDecoration(color: const Color(0xfffff5f6f9), borderRadius: BorderRadius.circular(30)),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30),
+                border: Border.all(color: kPrimaryLightColor)),
             child: AspectRatio(aspectRatio: 0.88, child: Image.network(productdetails.image)),
           ),
-          const SizedBox(width: 10),
           SizedBox(
             height: getProportionateScreenHeight(88),
             width: SizeConfig.screenWidth * 0.5,
-            child: FittedBox(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                FittedBox(
+                  child: Text(
                     productdetails.title,
                     maxLines: 2,
                     style: TextStyle(color: Colors.black, fontSize: getProportionateScreenHeight(20)),
                   ),
-                  const SizedBox(height: 5),
-                  Text.rich(TextSpan(
-                    text: '\$ ${productdetails.price.toString()}',
-                    style: TextStyle(color: kPrimaryColor, fontSize: getProportionateScreenHeight(15)),
-                    children: [
-                      TextSpan(
-                          text: ' X ${productdetails.quantity.toString()}', style: const TextStyle(color: kTextColor))
-                    ],
-                  )),
-                  Text(
-                    'size - ${productdetails.size}',
-                    maxLines: 1,
-                    style: TextStyle(fontSize: getProportionateScreenHeight(15)),
-                  ),
-                ],
-              ),
+                ),
+                const SizedBox(height: 5),
+                Text.rich(TextSpan(
+                  text: '\$ ${productdetails.price.toString()}',
+                  style: TextStyle(color: kPrimaryColor, fontSize: getProportionateScreenHeight(15)),
+                  children: [
+                    TextSpan(
+                        text: ' X ${productdetails.quantity.toString()}', style: const TextStyle(color: kTextColor))
+                  ],
+                )),
+                Text(
+                  'size - ${productdetails.size}',
+                  maxLines: 1,
+                  style: TextStyle(fontSize: getProportionateScreenHeight(15)),
+                ),
+              ],
             ),
           ),
+          const SizedBox(width: 10),
         ],
       ),
     );
